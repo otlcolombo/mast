30448f3775c65d5f48e37583e75a35525396dc64
Joseph Fonseka <hevayo@gmail.com>
2023-02-04 12:31:22 +0530

    Add react router and MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import Orders from "./pages/Orders";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter basename="/mast">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="blogs" element={<Orders />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
